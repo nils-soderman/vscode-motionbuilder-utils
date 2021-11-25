b211ba1f11ec3e84ca5e9854486fd5efa35cfc61
Nils Söderman <rymdnisse@live.com>
2021-11-25 20:31:40 +0100

    Fixed python var `__file__` not being set when executing scripts

---
+++ python/mb-execute.py
@@ -0,0 +1,9 @@
+import tempfile
+import os
+import json
+
+with open(os.path.join(tempfile.gettempdir(), "VSCode-MotionBuilder-Utils", 'vscode-exec.json'), 'r') as _VsCodeFile:
+    _VsCodeData = json.load(_VsCodeFile)
+    __file__ = _VsCodeData.get("__file__", "")
+    with open(_VsCodeData["file"], 'r') as _VsCodeFile: 
+        exec(_VsCodeFile.read())
--- src/execute-script.ts
+++ src/execute-script.ts
@@ -1,38 +1,62 @@
 import * as vscode from 'vscode';
 
+import * as path from 'path';
+
 import * as motionBuilderConsole from './motionbuilder-console';
 import * as utils from './utils';
 
 
 const TEMP_FILENAME = "VSCode-MotionBuilder-Exec";
+const TEMP_EXECDATA_FILENAME = "vscode-exec.json";
+const PYTHON_ENTER_FILE = path.join(__dirname, "..", "python", "mb-execute.py");
+
+
+/**
+ * Write a json temp file that can be read by MotionBuilder, to know what script to execute etc.
+ * @param fileToExecute The abs filepath to the .py file that should be executed
+ * @param originalFilepath The abs filepath to the source filepath, will be used to set the python var `__file__`
+ */
+function writeDataFile(fileToExecute: string, originalFilepath: string) {
+    let data: any = {};
+    data["file"] = fileToExecute;
+    data["__file__"] = originalFilepath;
+    utils.saveTempFile(TEMP_EXECDATA_FILENAME, JSON.stringify(data));
+}
 
 
 export async function execute() {
-    if (!vscode.window.activeTextEditor?.document) {
+    if (!vscode.window.activeTextEditor) {
         return;
     }
-
-    let fileToExecute = "";
-
+    const activeDocuemt = vscode.window.activeTextEditor.document;
     let selectionText = "";
+
+    // Combine all user selections into a single string
     for (let selection of vscode.window.activeTextEditor.selections) {
         if (!selection.isEmpty) {
-            selectionText += vscode.window.activeTextEditor.document.getText(selection) + "\n";
+            selectionText += activeDocuemt.getText(selection) + "\n";
         }
     }
 
+    let fileToExecute = "";
+    // If user had any selected text, save the selection as a temp file to execute
     if (selectionText) {
         fileToExecute = utils.saveTempFile(TEMP_FILENAME, selectionText);
     }
-    else if (vscode.window.activeTextEditor.document.isDirty) {
-        // File is dirty, save a copy in temp
-        fileToExecute = utils.saveTempFile(TEMP_FILENAME, vscode.window.activeTextEditor.document.getText());
-    }
-        else {
-        fileToExecute = vscode.window.activeTextEditor.document.uri.fsPath;
+
+    else if (activeDocuemt.isDirty) {
+        // File is dirty, save a copy of the currenet text in temp
+        fileToExecute = utils.saveTempFile(TEMP_FILENAME, activeDocuemt.getText());
     }
 
-    // TODO: This can fail if file is currently saving
-    motionBuilderConsole.runCommand(`with open(r'${fileToExecute}', 'r') as f:exec(f.read())\n`);
+    else {
+        fileToExecute = activeDocuemt.uri.fsPath;
+    }
+
+    // File an info file telling mb what script to run, etc.
+    writeDataFile(fileToExecute, activeDocuemt.uri.fsPath);
+
+    motionBuilderConsole.runCommand(`with open(r'${PYTHON_ENTER_FILE}','r')as f:exec(f.read())\n`);
+
 }
 
