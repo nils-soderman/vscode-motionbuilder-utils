10a2160a9bcbc5a5c3abe93ec1fd8b02f65c4d78
Nils Söderman <rymdnisse@live.com>
2021-12-15 21:20:07 +0100

    backup of the stub-file generator

--- build-scripts/generate-stub.py
+++ build-scripts/generate-stub.py
@@ -23,45 +23,23 @@ import pyfbsdk
 
 MODULES = [pyfbsdk, pyfbsdk_additions, pythonidelib]
 
-
 OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "..", "auto-completion")
 
+TAB_CHAR = "    "
 
-# --------------------------------------------------------
-#       Generate file from python's Help() function
-# --------------------------------------------------------
-
-def GenerateHelpFile(filepath, moduleName):
-    with open(filepath, 'w+') as File:
-        sys.stdout = File
-        pydoc.help(moduleName)
-        sys.stdout = sys.__stdout__
-    
-    return filepath
-
-
-def GenerateMotionBuilderHelpFiles():
-    GenerateHelpFile(os.path.join(OUTPUT_DIR, "pydbsdk_help.txt"), "pyfbsdk")
-    GenerateHelpFile(os.path.join(OUTPUT_DIR, "pydbsdk_additions_help.txt"), "pyfbsdk_additions")
-    GenerateHelpFile(os.path.join(OUTPUT_DIR, "pythonidelib_help.txt"), "pythonidelib")
-
+# Custom additions to insert at the top of the stub file
+CUSTOM_ADDITIONS_FILEPATH =  os.path.join(os.path.dirname(__file__), "stub-custom-additions.py")
 
 # --------------------------------------------------------
 #              Native pyfbsdk generated docs
 # --------------------------------------------------------
-
-def GetMotionBuilderInstallationDirectory():
-    return os.path.dirname(os.path.dirname(os.path.dirname(sys.executable)))
-
-def GetMotionBuilderPythonConfigDir():
-    return os.path.join(GetMotionBuilderInstallationDirectory(), "bin", "config", "Python")
-
 
 def PatchGeneratedDocString(Text):
     # Remove HTML tags
     for TagName, ReplaceWith in [("b", "")]:
         Text = Text.replace("<%s>" % TagName, ReplaceWith)
         Text = Text.replace("</%s>" % TagName, ReplaceWith)
+    Text = Text.replace("b>", "") # There are some broken HTML tags in there too
         
     # Patch code
     if "@code" in Text:
@@ -76,8 +54,11 @@ def PatchGeneratedDocString(Text):
                     Line = "\n"
                 elif not Line.strip():
                     continue
-                elif not bFirstCodeLine:
-                    Line = "    %s" % Line
+                else:
+                    if Line.strip().startswith("//"):
+                        Line = Line.replace("//", "#")
+                    if not bFirstCodeLine:
+                        Line = "    %s" % Line
                 bFirstCodeLine = False
             elif Line.startswith("@code"):
                 bFirstCodeLine = True
@@ -100,6 +81,7 @@ def PatchArgumentName(Param:str):
             Param = Param[1:]
     return Param
 
+
 def GetArgumentsFromFunction(Function):
     HelpFunction = Function.__doc__.split("->", 1)[0]
     HelpArgumentString = HelpFunction.split("(", 1)[1].strip()[:-1]
@@ -112,6 +94,7 @@ def GetArgumentsFromFunction(Function):
         Type, ArgName = Argument.strip().split(")")
         ReturnValue.append((ArgName.strip(), Type[1:].strip()))
     return ReturnValue
+
 
 def GenerateStubFunction(Function, DocMembers):
     DocFunctions = [x for x in DocMembers if type(x).__name__ == "function"]
@@ -150,19 +133,107 @@ def GenerateStubFunction(Function, DocMembers):
     
     # Doc String
     if DocRef:
-        functionString += '\n\t"""%s"""\n\t' % PatchGeneratedDocString(DocRef.__doc__)
+        functionString += '\n    """%s"""\n    ' % PatchGeneratedDocString(DocRef.__doc__)
     
     functionString += "..."
     
     return functionString
 
 
+def GetClassParent(Class):
+    Parents = Class.__bases__
+    if len(Parents) > 1:
+        raise Exception("Support for multiple parent classes not yed implemented")
+    return Parents[0]
+
+def GetClassParentName(Class):
+    ParentClassName = GetClassParent(Class).__name__
+    if ParentClassName == "instance":
+        ParentClassName = ""
+
+    elif ParentClassName == "enum":
+        ParentClassName = "_Enum"
+    return ParentClassName
+
+
+def GetClassMembers(Class):
+    IgnoreMembers = ["names", "values", "__slots__", "__instance_size__"]
+    Members = inspect.getmembers(Class)
+    ParentClass = GetClassParent(Class)
+    UniqueMemebers = [x for x in Members if not hasattr(ParentClass, x[0]) and x[0] not in IgnoreMembers and not x[0].startswith("__")]
+    return UniqueMemebers
+
+def GenerateStubClass(Class, DocMembers):
+    ClassName:str = Class.__name__
+    DocClasses = [x for x in DocMembers if type(x).__name__ in ["class", "type"]]
+    DocMemberNames = [x.__name__ for x in DocClasses]
+    
+    ClassString = "class %s(%s):\n    " % (ClassName, GetClassParentName(Class))
+    
+    # Docs
+    DocGenRef = FindDocGenRef(DocMembers, ClassName)
+    DocGenMembers = []
+    if DocGenRef:
+        DocString = PatchGeneratedDocString(DocGenRef.__doc__)
+        if DocString:
+            ClassString += '"""%s"""\n    ' % DocString
+            
+        DocGenMembers = GetClassMembers(DocGenRef)
+    DocGenMembersDict = dict(DocGenMembers)
+    
+    # Class Memebers, functions & properties
+    ClassMemebers = GetClassMembers(Class)
+    for ClassMemeber in ClassMemebers:
+        MemberType = type(ClassMemeber[1]).__name__
+        if MemberType == "function":
+            FunctionString = GenerateStubFunction(ClassMemeber[1], DocGenMembers)
+            ClassString += "%s\n%s" % (FunctionString, TAB_CHAR)
+        elif MemberType == "property":
+            pass
+        else:
+            ClassString += "%s:_Enum\n%s" % (ClassMemeber[0], TAB_CHAR)
+            
+        if ClassMemeber[0] in DocGenMembersDict:
+            DocString = PatchGeneratedDocString(DocGenMembersDict.get(ClassMemeber[0]).__doc__)
+            if DocString:
+                ClassString += '"""%s"""\n%s' %(DocString, TAB_CHAR) 
+    
+    
+    ClassString += "..."
+    
+    return ClassString
+
+def FindDocGenRef(DocMembers, RefName):
+    for Mebmer in DocMembers:
+        if hasattr(Mebmer, "__name__") and Mebmer.__name__ == RefName:
+            return Mebmer
+
+def SortClasses(Classes):
+    """ 
+    Sort classes based on their parent class
+    """
+    i = 0
+    ClassNames = [x.__name__ for x in Classes]
+    while (i < len(Classes)):
+        ParentClassName = GetClassParentName(Classes[i])
+        if ParentClassName:
+            ParentClassIndex = ClassNames.index(ParentClassName)
+            if ParentClassIndex >= i:
+                Classes.insert(ParentClassIndex + 1, Classes.pop(i))
+                ClassNames.insert(ParentClassIndex + 1, ClassNames.pop(i))
+                i -= 1
+        
+        i += 1
+        
+    return Classes
 
 def GenerateStub(Filepath: str, Module, GeneratedDocModuleName = ""):
     Functions = [x[1] for x in inspect.getmembers(Module) if type(x[1]).__name__ == "function"]
-    Classes = [x for x in inspect.getmembers(Module) if type(x[1]).__name__ == "class"]
-    Types = [x for x in inspect.getmembers(Module) if type(x[1]).__name__ == "type"]
+    Classes = [x[1] for x in inspect.getmembers(Module) if type(x[1]).__name__ == "class"]
+    Types = [x[1] for x in inspect.getmembers(Module) if type(x[1]).__name__ == "type"]
     Misc = [x for x in inspect.getmembers(Module) if type(x[1]).__name__ not in ["function", "class", "type"]]
+
+    Classes = SortClasses(Classes)
     
     StubFileContent = ""
     DocMembers = []
@@ -170,12 +241,26 @@ def GenerateStub(Filepath: str, Module, GeneratedDocModuleName = ""):
         ImportedModule = importlib.import_module(GeneratedDocModuleName)
         DocMembers = [x[1] for x in inspect.getmembers(ImportedModule)]
     
+    if os.path.isfile(CUSTOM_ADDITIONS_FILEPATH):
+        with open(CUSTOM_ADDITIONS_FILEPATH, 'r') as File:
+            StubFileContent += "%s\n" % File.read()
+    
+    for Type in Types:
+        TypeString = GenerateStubClass(Type, DocMembers)
+        if TypeString:
+            StubFileContent += "%s\n" % TypeString
+            
+            
+    for Class in Classes:
+        ClassString = GenerateStubClass(Class, DocMembers)
+        if ClassString:
+            StubFileContent += "%s\n" % ClassString
+    
     for Function in Functions:
         FunctionString = GenerateStubFunction(Function, DocMembers)
         if FunctionString:
             StubFileContent += "%s\n" % FunctionString
-    
-        
+            
     
     with open(Filepath, "w+") as File:
         File.write(StubFileContent)
@@ -192,27 +277,3 @@ def MotionBuilderMain():
 
 # if "builtin" in __name__:
 MotionBuilderMain()
-
-
-
-
-
-# def GenerateStub(HelpFile, MBGeneratedStub):
-#     Content = {}
-#     CurrentItem = ""
-#     CurrentCategory = ""
-#     with open(HelpFile, "r") as File:
-#         for Line in File:
-#             if Line[0].isalpha():
-#                 # New category
-#                 CurrentCategory = Line.strip()
-#                 Content[CurrentCategory] = []
-#                 CurrentItem = ""
-#             elif Line.isspace():
-#                 # New Item
-#                 Content[CurrentCategory].append(CurrentItem)
-#                 CurrentItem = ""
-#             else:
-#                 CurrentItem += Line
-
-#     print(Content["FUNCTIONS"][1])
