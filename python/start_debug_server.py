41af138df88d0d56febb43d03d728e0c9363e488
Nils Söderman <rymdnisse@live.com>
2022-03-06 16:20:45 +0100

    Start debug server now supports mb 2020

--- python/start_debug_server.py
+++ python/start_debug_server.py
@@ -5,15 +5,26 @@ import json
 import sys
 import os
 
-SERVER_ENV_VAR = "MB_Utils_Debug_Enabled"
+VSCODE_DEBUG_SERVER_ENV_VAR = "MB_Utils_Debug_Enabled"
 
-PYTHON_EXECUTABLE = os.path.join(os.path.dirname(sys.executable), "mobupy.exe")
-ATTACH_SETTINGS_FILEPATH = os.path.join(tempfile.gettempdir(), "VSCode-MotionBuilder-Utils", 'vscode-attach.json')
+MOBU_PYTHON_EXECUTABLE = os.path.join(os.path.dirname(sys.executable), "mobupy.exe")
+VSCODE_MOBU_TEMPDIR = os.path.join(tempfile.gettempdir(), "VSCode-MotionBuilder-Utils")
 
 
-def GetSettings():
-    with open(ATTACH_SETTINGS_FILEPATH, 'r') as File:
+def GetVSCodeAttachSettings():
+    """ Get a dict with settings passed along from VSCode """
+    SettingsFilepath = os.path.join(VSCODE_MOBU_TEMPDIR, 'vscode-attach.json')
+    with open(SettingsFilepath, 'r') as File:
         return json.load(File)
+
+
+def IsModuleAvaliable(ModuleName):
+    """ Check if a python module can be imported """
+    try:
+        exec("import %s" % ModuleName)
+        return True
+    except:
+        return False
 
 
 def InstallAndImportModule(PackageName, Target = ""):
@@ -26,29 +37,58 @@ def InstallAndImportModule(PackageName, Target = ""):
                 sys.path.append(Target)
             return importlib.import_module(PackageName)
         except:
+            ArgTarget = ""
             if Target:
-                "--target=%s" % Target
-            subprocess.check_call([PYTHON_EXECUTABLE, "-m", "pip", "install", Target, PackageName])
+                ArgTarget = "--target=%s" % Target
+
+            # Ensure pip is avaliable
+            if not IsModuleAvaliable("pip"):
+                if not IsModuleAvaliable("ensurepip"):
+                    return False
+                
+                TempInstallationDir = os.path.join(VSCODE_MOBU_TEMPDIR, "TempPipInstall")
+                subprocess.check_call([MOBU_PYTHON_EXECUTABLE, "-m", "ensurepip", "--root", TempInstallationDir])
+
+                pipModulePath = ""
+                for Root, Directory, Files in os.walk(TempInstallationDir):
+                    if "__init__.py" in Files and os.path.basename(Root) == "pip":
+                        pipModulePath = os.path.join(Root)
+                        break
+
+                if not pipModulePath:
+                    return False
+
+                subprocess.check_call([MOBU_PYTHON_EXECUTABLE, pipModulePath, "install", ArgTarget, PackageName])
+            else: 
+                subprocess.check_call([MOBU_PYTHON_EXECUTABLE, "-m", "pip", "install", ArgTarget, PackageName])
 
     return importlib.import_module(PackageName)
 
 
 def IsDebugServerRunning():
-    return  os.environ.get(SERVER_ENV_VAR, "") == str(True)
+    """ Check if a debug server already has been started in this MB instance """
+    return os.environ.get(VSCODE_DEBUG_SERVER_ENV_VAR, "") == str(True)
 
 
 def EnableDebugServer():
     # Get settings
-    Settings = GetSettings()
+    Settings = GetVSCodeAttachSettings()
     Port = Settings.get("port")
     Target = Settings.get("target")
 
-    # Setup server
+    # Attempt to import / install debugpy
     debugpy = InstallAndImportModule("debugpy", Target)
-    debugpy.configure(python = PYTHON_EXECUTABLE)
+    if not debugpy:
+        print("ERROR: Failed to import/install debugpy.")
+        return
+
+    # Start the debugpy server
+    debugpy.configure(python = MOBU_PYTHON_EXECUTABLE)
     debugpy.listen(Port)
 
-    os.environ[SERVER_ENV_VAR] = str(True)
+    
+
+    os.environ[VSCODE_DEBUG_SERVER_ENV_VAR] = str(True)
 
 
 def main():
