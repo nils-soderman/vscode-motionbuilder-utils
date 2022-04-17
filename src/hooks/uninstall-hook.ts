1fa251742eb4031dfddbdb37bf068485ff45995e
Nils Söderman <contact@nilssoderman.com>
2022-04-17 08:16:23 +0200

    Fixed the uninstall-hook to make sure it doesn't import any non-native nodejs modules

--- src/hooks/uninstall-hook.ts
+++ src/hooks/uninstall-hook.ts
@@ -1,22 +1,45 @@
 /**
- * This file get's called when the extension is completely uninstalled.
+ * This scripts get's called once this extension has been uninstalled & VS Code restarted (not reloaded).
+ * This get's called outside of VS Code's environment & without the extension being loaded,
+ * Therefore any custom modules or the vscode api cannot be reached here, only import native nodejs modules! 
  */
 
-import * as fs    from 'fs';
+import * as fs from 'fs';
+import * as path from 'path';
 
-import * as utils from '../utils';
 
+/** --------------------------------------------------------------------------
+ *                      Functions lifted from Utils
+ * -------------------------------------------------------------------------- */
+
+const TEMPFOLDER_NAME = "VSCode-MotionBuilder-Utils";
+
+export function getAppDataFolder() {
+    return process.env.APPDATA || (process.platform === 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");
+}
+
+export function getExtensionAppdataFolder(bEnsureExists = true) {
+    const appdataDir = path.join(getAppDataFolder(), TEMPFOLDER_NAME);
+    if (bEnsureExists && !fs.existsSync(appdataDir)) {
+        fs.mkdirSync(appdataDir);
+    }
+    return appdataDir;
+}
+
+
+/** --------------------------------------------------------------------------
+ *                                  Main
+ * -------------------------------------------------------------------------- */
 
 function main() {
-    // Clean up any temp files
-    utils.cleanupTempFiles();
-    
     // Cleanup all files placed under %AppData%
-    const appdataFolder = utils.getExtensionAppdataFolder();
+    const appdataFolder = getExtensionAppdataFolder();
     if (fs.existsSync(appdataFolder)) {
         fs.rmSync(appdataFolder, { recursive: true });
     }
 }
 
 
-main();
+if (require.main === module) {
+    main();
+}
