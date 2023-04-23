02a788390c2e3c09bda1742351dbf684ee803224
Nils Söderman <rymdnisse@live.com>
2023-04-23 10:20:03 +0200

    Removed config listener

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,10 @@
 import * as vscode from 'vscode';
 
+import * as codeCompletion from './scripts/code-completion';
 import * as documentation from './scripts/documentation';
-import * as mobuDebugger  from './scripts/debugger';
-import * as stubFiles 	  from './scripts/code-completion';
-import * as execute 	  from './scripts/execute-script';
-import * as utils 		  from './modules/utils';
+import * as mobuDebugger from './scripts/debugger';
+import * as execute from './scripts/execute-script';
+import * as utils from './modules/utils';
 
 
 export function activate(context: vscode.ExtensionContext) {
@@ -26,7 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('motionbuilder.setupCodeCompletion', () => {
 			// Setup the stub files
-			stubFiles.setup();
+			codeCompletion.setup();
 		})
 	);
 
@@ -41,48 +41,10 @@ export function activate(context: vscode.ExtensionContext) {
 			documentation.browseExamples();
 		})
 	);
-
-	
-	// -------------------------------
-	// 		  	  Events
-	// -------------------------------
-
-	context.subscriptions.push(
-		vscode.workspace.onDidChangeConfiguration(onConfigurationChanged)
-	);
 }
 
 
-/**
- * Called when some configuration has been changed.
- * @param event The vscode configuration event
- */
-async function onConfigurationChanged(event: vscode.ConfigurationChangeEvent) {
-	// Early out if no motionbuilder configuration has been updated
-	if (!event.affectsConfiguration("motionbuilder")) {
-		return;
-	}
-
-	let bReloadRequired = false;
-	
-	// If the MotionBuilder version was changed, update the stub-files
-	if (event.affectsConfiguration("motionbuilder.version")) {
-		await stubFiles.setup(true);
-		bReloadRequired = true;
-	}
-
-	// If needed, ask user to reload VS Code
-	if (bReloadRequired) {
-		let selection = await vscode.window.showInformationMessage("Reload required for changes to take full effect", "Reload", "Close");
-		if (selection == "Reload") {
-			vscode.commands.executeCommand("workbench.action.reloadWindow");
-		}
-	}
-
-}
-
-
-export function deactivate() { 
+export function deactivate() {
 	// Remove all temp files created by this extension
 	utils.cleanupTempFiles();
 }
