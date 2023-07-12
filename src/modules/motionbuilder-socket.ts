2e4c2cd36c58b3e0227622a3f20253bec4c3238f
Nils Söderman <contact@nilssoderman.com>
2023-07-12 13:23:03 +0200

    Added `isOpen` method

--- src/modules/motionbuilder-socket.ts
+++ src/modules/motionbuilder-socket.ts
@@ -4,7 +4,7 @@ export class MotionBuilderSocket {
     private socket?: net.Socket;
     private isReady = false;
 
-    private commandQueue: { command: string, resolve: (value: string) => void, reject: (error: Error) => void }[] = [];
+    private writeQueue: { command: string, resolve: (value: string) => void, reject: (error: Error) => void }[] = [];
     private isExecuting = false;
 
     constructor(
@@ -28,10 +28,70 @@ export class MotionBuilderSocket {
 
     private onClose() {
         this.socket = undefined;
+        this.isReady = false;
     }
 
-    public on(event: 'close', listener: (...args: any[]) => void) {
+
+    /**
+     * Write to the socket and wait for a response.
+     * @param buffer The data to write to the socket
+     * @returns 
+     */
+    private write(buffer: string | Uint8Array): Promise<string> {
+        return new Promise((resolve, reject) => {
+            if (!this.isOpen()) {
+                reject(new Error('Socket is not ready, you must wait for the open promise to resolve.'));
+                return;
+            }
+
+            const commandPromise = { command: buffer.toString(), resolve, reject };
+            this.writeQueue.push(commandPromise);
+
+            if (!this.isExecuting) {
+                this.executeNextCommand().catch(console.error);
+            }
+        });
+    }
+
+    private executeNextCommand(): Promise<void> {
+        return new Promise((resolve) => {
+            if (this.writeQueue.length === 0) {
+                this.isExecuting = false;
+                resolve();
+                return;
+            }
+
+            this.isExecuting = true;
+
+            const { command, resolve: commandResolve, reject: commandReject } = this.writeQueue.shift()!;
+
+            this.socket?.write(command, (error) => {
+                if (error) {
+                    commandReject(error);
+                    this.executeNextCommand().catch(console.error);
+                    return;
+                }
+            });
+
+            let recivedBuffer = '';
+            this.socket?.on('data', (chunk: Buffer) => {
+                recivedBuffer += chunk.toString('utf8');
+
+                if (recivedBuffer.trimEnd().endsWith('>>>')) {
+                    this.socket?.removeAllListeners('data');
+                    commandResolve(recivedBuffer);
+                    this.executeNextCommand().catch(console.error);
+                }
+            });
+        });
+    }
+
+    on(event: 'close', listener: (...args: any[]) => void) {
         this.socket?.on(event, listener);
+    }
+
+    isOpen(): boolean {
+        return this.socket !== undefined && this.socket.writable && this.isReady;
     }
 
     /**
@@ -68,59 +128,6 @@ export class MotionBuilderSocket {
         this.socket?.destroy();
     }
 
-    /**
-     * Write to the socket and wait for a response.
-     * @param buffer The data to write to the socket
-     * @returns 
-     */
-    private write(buffer: string | Uint8Array): Promise<string> {
-        return new Promise((resolve, reject) => {
-            if (!this.socket || !this.isReady) {
-                reject(new Error('Socket is not ready, you must wait for the open promise to resolve.'));
-                return;
-            }
-
-            const commandPromise = { command: buffer.toString(), resolve, reject };
-            this.commandQueue.push(commandPromise);
-
-            if (!this.isExecuting) {
-                this.executeNextCommand().catch(console.error);
-            }
-        });
-    }
-
-    private executeNextCommand(): Promise<void> {
-        return new Promise((resolve) => {
-            if (this.commandQueue.length === 0) {
-                this.isExecuting = false;
-                resolve();
-                return;
-            }
-
-            this.isExecuting = true;
-
-            const { command, resolve: commandResolve, reject: commandReject } = this.commandQueue.shift()!;
-
-            this.socket?.write(command, (error) => {
-                if (error) {
-                    commandReject(error);
-                    this.executeNextCommand().catch(console.error);
-                    return;
-                }
-            });
-
-            let recivedBuffer = '';
-            this.socket?.on('data', (chunk: Buffer) => {
-                recivedBuffer += chunk.toString('utf8');
-
-                if (recivedBuffer.trimEnd().endsWith('>>>')) {
-                    this.socket?.removeAllListeners('data');
-                    commandResolve(recivedBuffer);
-                    this.executeNextCommand().catch(console.error);
-                }
-            });
-        });
-    }
 
     /**
      * Execute a python statement in MotionBuilder.
