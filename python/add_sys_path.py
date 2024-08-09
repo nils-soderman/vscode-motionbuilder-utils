84da9f658a8e7b8afc31d1b53493e81764528f28
Nils Söderman <rymdnisse@live.com>
2024-08-09 21:18:04 +0200

    `add_sys_path.py` - Added Python 2 support

--- python/add_sys_path.py
+++ python/add_sys_path.py
@@ -5,16 +5,19 @@ Script to add the given paths to sys.path
 import sys
 import os
 
+
 def main():
     vsc_paths = globals().get("vsc_paths", [])
 
-    vsc_paths.sort(key=len, reverse=True)  # Sort the paths by length to ensure that the most specific paths are added first
+    # Sort the paths by length to ensure that the most specific paths are added first
+    vsc_paths.sort(key=len, reverse=True)
 
     for vsc_path in vsc_paths:
         normalized_path = os.path.normpath(vsc_path)
         # Make sure the path doesn't already exist in sys.path
         if not any(normalized_path.lower() == os.path.normpath(path).lower() for path in sys.path):
             sys.path.append(normalized_path)
-            print(f'Added "{normalized_path}" to sys.path')
+            print('Added "%s" to sys.path' % normalized_path)
+
 
 main()
